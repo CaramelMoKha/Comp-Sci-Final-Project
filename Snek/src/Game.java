93f76604bdb7b3ab61979096f009a5969480bc3c
CaramelMoKha <mojangles2000@gmail.com>
2017-05-15 12:02:49 -0400

    working on main

--- Snek/src/Game.java
+++ Snek/src/Game.java
@@ -6,20 +6,54 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Game extends JPanel implements KeyListener
+public class Game extends JPanel
 {
 	private static final long serialVersionUID = 1L;
 	
 	private long score;
-	private Color[][] screen;
-	private Snek snek;
+	private static Color[][] screen;
+	private static Snek snek;
 	
 	public Game()
 	{
 		snek = new Snek(20, 20);
-		this.addKeyListener(this);
 		this.setFocusable(true);
 		this.requestFocusInWindow();
+	}
+	
+	// Creates a border around the screen
+	public void screen() 
+	{
+		screen = new Color[40][40];
+		for (int i = 0; i < 40; i++) 
+		{ 
+			for (int j = 0; j < 39; j++) 
+			{
+				if (i == 0 || i == 39 || j == 38) 
+					screen[i][j] = new Color(255, 0, 0).darker();
+				else 
+					screen[i][j] = Color.BLACK;
+			}
+		}
+		screen[snek.getX()][snek.getY()] = Color.BLUE;
+	}
+	
+	public void paintComponent(Graphics g)
+	{
+		// Paint the screen
+		g.fillRect(0, 0, 26*40, 26*39);
+		for (int i = 0; i < 40; i++) 
+		{
+			for (int j = 0; j < 39; j++) 
+			{
+				g.setColor(screen[i][j]);
+				g.fillRect(26*i, 26*j, 25, 25);
+			}
+		}
+		
+		// Display the score
+		g.setColor(Color.WHITE);
+		g.drawString("" + score, 30, 25);
 	}
 	
 	public static void main(String[] args)
@@ -72,39 +106,5 @@ public class Game extends JPanel implements KeyListener
 				}
 			}
 		}
-	}
-	// Creates a border around the screen
-	public void screen() 
-	{
-		screen = new Color[40][40];
-		for (int i = 0; i < 40; i++) 
-		{ 
-			for (int j = 0; j < 39; j++) 
-			{
-				if (i == 0 || i == 39 || j == 38) 
-					screen[i][j] = new Color(255, 0, 0).darker();
-				else 
-					screen[i][j] = Color.BLACK;
-			}
-		}
-		screen[snek.getX()][snek.getY()] = Color.BLUE;
-	}
-	
-	public void paintComponent(Graphics g)
-	{
-		// Paint the screen
-		g.fillRect(0, 0, 26*40, 26*39);
-		for (int i = 0; i < 40; i++) 
-		{
-			for (int j = 0; j < 39; j++) 
-			{
-				g.setColor(screen[i][j]);
-				g.fillRect(26*i, 26*j, 25, 25);
-			}
-		}
-		
-		// Display the score
-		g.setColor(Color.WHITE);
-		g.drawString("" + score, 30, 25);
 	}
 }
